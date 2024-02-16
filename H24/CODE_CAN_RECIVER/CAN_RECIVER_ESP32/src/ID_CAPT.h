333bfcd7679b02f5a0d90a8748f4416aa406c946
FFXIII <89463083+FFXIII@users.noreply.github.com>
2024-02-16 16:03:59 -0500

    J'ai bien mis les fct du CAN dans CAN_FCT.cpp

--- H24/CODE_CAN_RECIVER/CAN_RECIVER_ESP32/src/ID_CAPT.h
+++ H24/CODE_CAN_RECIVER/CAN_RECIVER_ESP32/src/ID_CAPT.h
@@ -1,11 +1,11 @@
 /*
-Programme : main.cpp
+Programme : ID_CAPT.h
 Auteur :    Marc-Étienne Gendron-Fontaine
 Date :      12 février 2024
-Brief :     
+Brief :     Ce fichier d'entête contient tous les ID des capteurs sous forme de constante.
 
-Matérielle:  
-Encironement:
+Matérielle: ESP32-S3 (x2), TJA1050 (x2)
+Encironement: Visual Studio Code V 1.86.2, Système d’exploitation : Linux x64 5.15.0-94-generic.
 */
 
 
