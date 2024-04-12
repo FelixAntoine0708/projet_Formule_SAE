9ad042c950884697222d35c325f0b11320d7b340
FFXIII <201604617@cegepsherbrooke.qc.ca>
2024-04-12 16:18:15 -0400

    Création de l'affichage final

---
+++ H24/LOGICIEL/AFFICHAGE_SAE/AFFICHAGE_SAE_CAN/AFFICHAGE_SAE_CAN_V_0_0_1/AFFICHAGE_SAE_CAN_V_0_0_1.ino
@@ -0,0 +1,226 @@
+//
+// NB! This is a file generated from the .4Dino file, changes will be lost
+//     the next time the .4Dino file is built
+//
+#include "gfx4desp32_gen4_ESP32_70CT.h"
+
+gfx4desp32_gen4_ESP32_70CT gfx = gfx4desp32_gen4_ESP32_70CT();
+
+#include "AFFICHAGE_SAE_CAN_V_0_0_1Const.h"    // Note. This file will not be created if there are no generated graphics
+#include <ESP32-TWAI-CAN.hpp>
+
+#define CAN_TX    43
+#define CAN_RX    44
+
+#define TEMP_SET_3  0xA2
+
+// VARIALBLE GLOBALE
+CanFrame rxFrame;   // Pour la trame CAN
+
+int motor_temp = 0;
+int coolant_temp = 0;
+
+void setup()
+{
+  gfx.begin();
+  gfx.Cls();
+  gfx.ScrollEnable(false);
+  gfx.BacklightOn(true);
+  gfx.Orientation(LANDSCAPE);
+  gfx.SmoothScrollSpeed(5);
+  gfx.TextColor(WHITE, BLACK); gfx.Font(2);  gfx.TextSize(1);
+  gfx.Open4dGFX("AFFICHAGE_SAE_CAN_V_0_0_1"); // Opens DAT and GCI files for read using filename without extension.
+  gfx.touch_Set(TOUCH_ENABLE);                // Global touch enabled
+
+  // CAN -----------------------------------
+  ESP32Can.setPins(CAN_TX, CAN_RX);
+  ESP32Can.setSpeed(ESP32Can.convertSpeed(500));
+  ESP32Can.setRxQueueSize(1);
+  ESP32Can.setTxQueueSize(1);
+  if(ESP32Can.begin())
+  {
+      gfx.println("CAN bus started!");
+      delay(2000);
+  }
+  else
+  {
+      gfx.println("CAN bus failed!");
+      while(1);
+  }
+
+
+  gfx.UserImages(iGauge1,0) ;                                // init_Gauge1 show initialy, if required
+  gfx.UserImages(iGauge2,0) ;                                // init_Gauge2 show initialy, if required
+  gfx.UserImages(iGauge3,0) ;                                // init_Gauge3 show initialy, if required
+  gfx.UserImage(iLeddigits1);                                // init_Leddigits1 show all digits at 0, only do this once
+  gfx.UserImage(iLeddigits2);                                // init_Leddigits2 show all digits at 0, only do this once
+  gfx.UserImage(iLeddigits3);                                // init_Leddigits3 show all digits at 0, only do this once
+  gfx.imageTouchEnable(iWinbutton1, true, MOMENTARY) ;       // init_Winbutton1(HV) enable touch of widget (on Form2)
+  gfx.UserImages(iWinbutton1,0) ;                            // init_Winbutton1(HV) show initially, if required (on Form2)
+  gfx.imageTouchEnable(iWinbutton2, true, MOMENTARY) ;       // init_Winbutton2 enable touch of widget (on Form2)
+  gfx.UserImages(iWinbutton2,0) ;                            // init_Winbutton2 show initially, if required (on Form2)
+  gfx.imageTouchEnable(iWinbutton3, true, MOMENTARY) ;       // init_Winbutton3 enable touch of widget (on Form2)
+  gfx.UserImages(iWinbutton3,0) ;                            // init_Winbutton3 show initially, if required (on Form2)
+  gfx.UserImage(iLeddigits4);                                // init_Leddigits4 show all digits at 0, only do this once
+  gfx.UserImages(iAngularmeter1,0) ;                         // init_Angularmeter1 show initialy, if required
+  gfx.UserImages(iGauge4,0) ;                                // init_Gauge4 show initialy, if required
+  gfx.UserImage(iLeddigits8);                                // init_Leddigits8 show all digits at 0, only do this once
+  gfx.UserImage(iLeddigits7);                                // init_Leddigits7 show all digits at 0, only do this once
+  gfx.UserImage(iLeddigits6);                                // init_Leddigits6 show all digits at 0, only do this once
+  gfx.UserImage(iLeddigits5);                                // init_Leddigits5 show all digits at 0, only do this once
+} // end Setup **do not alter, remove or duplicate this line**
+
+void loop()
+{
+
+   if(ESP32Can.readFrame(rxFrame, 25))
+   {
+      if(rxFrame.identifier == TEMP_SET_3)
+      {
+         coolant_temp = rxFrame.data[0] + rxFrame.data[1]<<8;
+         motor_temp = rxFrame.data[4] + rxFrame.data[5]<<8;
+      }
+
+   }
+
+
+
+
+
+
+
+
+
+  /*// put your main code here, to run repeatedly:
+  int itouched, val ;
+  if(gfx.touch_Update())
+  {
+    itouched = gfx.imageTouched() ;
+    switch (itouched)
+    {                                                         // start touched selection **do not alter, remove or duplicate this line**
+      // case statements for Knobs and Sliders go here
+      default :                                               // end touched selection **do not alter, remove or duplicate this line**
+        int button = gfx.ImageTouchedAuto();    // use default for keyboards and buttons
+        val = gfx.getImageValue(button);
+        switch (button)
+        {                                                     // start button selection **do not alter, remove or duplicate this line**
+          // case, one for each button or keyboard, default should end up as -1
+          case iWinbutton1 :                                      // process_Winbutton1(HV) process Button (on Form2)
+            // process win button, for toggle val will be 1 for down and 0 for up
+            break ;
+          case iWinbutton2 :                                      // process_Winbutton2 process Button (on Form2)
+            // process win button, for toggle val will be 1 for down and 0 for up
+            break ;
+          case iWinbutton3 :                                      // process_Winbutton3 process Button (on Form2)
+            // process win button, for toggle val will be 1 for down and 0 for up
+            break ;
+        }                                                     // end button selection **do not alter, remove or duplicate this line**
+    }
+  }  */
+}
+
+
+
+
+
+
+/*
+Brief   : Affiche la page Température
+Param   : Rien
+Return  : Rien
+*/
+void show_temp()
+{
+   gfx.Cls();  // Efface l'écran
+
+  // Gauge
+  gfx.UserImages(iGauge1, motor_temp); // MOTOR TEMP
+  gfx.UserImages(iGauge2, 0); // where frame is 0 to 100 (for a displayed 0 to 100)
+  gfx.UserImages(iGauge3, coolant_temp); // COOLANT TEMP
+
+  // TXT LABEL
+  gfx.UserImage(iStatictext4) ;  // Statictext4
+  gfx.UserImage(iStatictext3) ;  // Statictext3
+  gfx.UserImage(iStatictext2) ;  // Statictext2
+  gfx.UserImage(iStatictext1) ;  // Statictext1
+
+  // DIGIT
+  gfx.LedDigitsDisplay(motor_temp, iiLeddigits1, 4, 3, 39, 0) ;  // Leddigits1
+  gfx.LedDigitsDisplay(0, iiLeddigits2, 4, 3, 39, 0) ;  // Leddigits2
+  gfx.LedDigitsDisplay(coolant_temp, iiLeddigits3, 4, 3, 45, 0) ;  // Leddigits3
+
+
+  // BTN AFF
+  gfx.UserImages(iWinbutton1,0) ;
+  gfx.UserImages(iWinbutton2,0) ;
+  gfx.UserImages(iWinbutton3,0) ;
+
+  // BTN SETUP
+  gfx.imageTouchEnable(iWinbutton1, true);
+  gfx.imageTouchEnable(iWinbutton2, false);
+  gfx.imageTouchEnable(iWinbutton3, true);
+
+}
+
+
+
+
+
+/*
+Brief   : Affiche la page LV
+Param   : Rien
+Return  : Rien
+*/
+void showLV(){
+
+  gfx.Cls();  // Efface l'écran
+
+  // Widget
+  gfx.LedDigitsDisplay(numx, iiLeddigits4, 4, 3, 51, 0) ;  // Leddigits4
+  gfx.UserImages(iAngularmeter1, frame) ; // where frame is 0 to 100 (for a displayed 0 to 100)
+  gfx.UserImages(iGauge4, frame) ; // where frame is 0 to 100 (for a displayed 0 to 100)
+
+  // TXT LABEL
+  gfx.UserImage(iStatictext5) ;  // Statictext5
+  gfx.UserImage(iStatictext8) ;  // Statictext8
+  gfx.UserImage(iStatictext7) ;  // Statictext7
+  gfx.UserImage(iStatictext6) ;  // Statictext6
+
+    // BTN AFF
+  gfx.UserImages(iWinbutton1,0) ;
+  gfx.UserImages(iWinbutton2,0) ;
+  gfx.UserImages(iWinbutton3,0) ;
+
+  // BTN SETUP
+  gfx.imageTouchEnable(iWinbutton1, true);
+  gfx.imageTouchEnable(iWinbutton2, true);
+  gfx.imageTouchEnable(iWinbutton3, false);
+
+
+}
+
+
+
+/*
+Brief   : Affiche la page HV
+Param   : Rien
+Return  : Rien
+*/
+void showHV(){
+
+  gfx.Cls();  // Efface l'écran
+
+  // WIDGET
+  gfx.LedDigitsDisplay(0, iiLeddigits8, 4, 3, 42, 0) ;  // Leddigits8
+  gfx.LedDigitsDisplay(0, iiLeddigits7, 4, 3, 37, 0) ;  // Leddigits7
+  gfx.LedDigitsDisplay(0, iiLeddigits6, 4, 3, 41, 0) ;  // Leddigits6
+  gfx.LedDigitsDisplay(0, iiLeddigits5, 4, 3, 43, 0) ;  // Leddigits5
+
+  // TXT LABEL
+
+
+
+
+}
+
+
---
+++ H24/LOGICIEL/AFFICHAGE_SAE/AFFICHAGE_SAE_CAN/AFFICHAGE_SAE_CAN_V_0_0_1/AFFICHAGE_SAE_CAN_V_0_0_1Const.h
@@ -0,0 +1,46 @@
+// File generated 2024-04-12 15:22:30
+// Warning! This is a generated file, any manual changes will be
+// lost during the next generation.
+
+#define  IFONT_OFFSET     0
+// object indexes into ImageControl
+#define iStatictext9            0 // offset 0x0
+#define iLeddigits5             1 // offset 0xE00
+#define iiLeddigits5            2 // offset 0x6E00
+#define iLeddigits6             3 // offset 0x15E00
+#define iiLeddigits6            4 // offset 0x1C200
+#define iLeddigits7             5 // offset 0x2C000
+#define iiLeddigits7            6 // offset 0x30C00
+#define iLeddigits8             7 // offset 0x3C800
+#define iiLeddigits8            8 // offset 0x43000
+#define iWinbutton1             9 // offset 0x52E00
+#define iWinbutton2            10 // offset 0x57000
+#define iWinbutton3            11 // offset 0x5B400
+#define iGauge1                12 // offset 0x5F600
+#define iLeddigits1            13 // offset 0x2F1E00
+#define iiLeddigits1           14 // offset 0x2F7200
+#define iStatictext1           15 // offset 0x304200
+#define iGauge2                16 // offset 0x306800
+#define iLeddigits2            17 // offset 0x577A00
+#define iiLeddigits2           18 // offset 0x57CE00
+#define iStatictext2           19 // offset 0x58A200
+#define iGauge3                20 // offset 0x58C600
+#define iLeddigits3            21 // offset 0x7F5400
+#define iiLeddigits3           22 // offset 0x7FA600
+#define iStatictext3           23 // offset 0x807200
+#define iStatictext4           24 // offset 0x80A200
+#define iStatictext5           25 // offset 0x80BC00
+#define iGauge4                26 // offset 0x80C800
+#define iStatictext6           27 // offset 0xA32A00
+#define iAngularmeter1         28 // offset 0xA33C00
+#define iStatictext7           29 // offset 0x1337E00
+#define iLeddigits4            30 // offset 0x1338C00
+#define iiLeddigits4           31 // offset 0x133FA00
+#define iStatictext8           32 // offset 0x1350A00
+
+
+static const bool inputs[] PROGMEM = { false, false, false, false, false, false, false, false, 
+                  false, true, true, true, false, false, false, false, 
+                  false, false, false, false, false, false, false, false, 
+                  false, false, false, false, false, false, false, false, 
+                  false} ;
