46602485a10228d2cf2cdcb8598cc7ff677513e2
FFXIII <201604617@cegepsherbrooke.qc.ca>
2024-04-14 20:42:45 -0400

    UPDATE

--- H24/LOGICIEL/AFFICHAGE_SAE/AFFICHAGE_SAE_CAN/AFFICHAGE_SAE_CAN_V_0_0_1/AFFICHAGE_SAE_CAN_V_0_0_1Const.h
+++ H24/LOGICIEL/AFFICHAGE_SAE/AFFICHAGE_SAE_CAN/AFFICHAGE_SAE_CAN_V_0_0_1/AFFICHAGE_SAE_CAN_V_0_0_1Const.h
@@ -1,4 +1,4 @@
-// File generated 2024-04-14 16:13:23
+// File generated 2024-04-14 20:08:27
 // Warning! This is a generated file, any manual changes will be
 // lost during the next generation.
 
@@ -24,23 +24,23 @@
 #define iLeddigits1            17 // offset 0x2F6400
 #define iiLeddigits1           18 // offset 0x2FB800
 #define iStatictext1           19 // offset 0x308800
-#define iGauge2                20 // offset 0x30AE00
-#define iLeddigits2            21 // offset 0x57C000
-#define iiLeddigits2           22 // offset 0x581400
-#define iStatictext2           23 // offset 0x58E800
-#define iGauge3                24 // offset 0x590C00
-#define iLeddigits3            25 // offset 0x7F9A00
-#define iiLeddigits3           26 // offset 0x7FEC00
-#define iStatictext3           27 // offset 0x80B800
-#define iStatictext4           28 // offset 0x80E800
-#define iStatictext5           29 // offset 0x810200
-#define iGauge4                30 // offset 0x810E00
-#define iStatictext6           31 // offset 0xA37000
-#define iAngularmeter1         32 // offset 0xA38200
-#define iStatictext7           33 // offset 0x133C400
-#define iLeddigits4            34 // offset 0x133D200
-#define iiLeddigits4           35 // offset 0x1344000
-#define iStatictext8           36 // offset 0x1355000
+#define iGauge2                20 // offset 0x30AC00
+#define iLeddigits2            21 // offset 0x57BE00
+#define iiLeddigits2           22 // offset 0x581200
+#define iStatictext2           23 // offset 0x58E600
+#define iGauge3                24 // offset 0x590800
+#define iLeddigits3            25 // offset 0x7F9600
+#define iiLeddigits3           26 // offset 0x7FE800
+#define iStatictext3           27 // offset 0x80B400
+#define iStatictext4           28 // offset 0x80DE00
+#define iStatictext5           29 // offset 0x80F800
+#define iGauge4                30 // offset 0x810400
+#define iStatictext6           31 // offset 0xA36600
+#define iAngularmeter1         32 // offset 0xA37800
+#define iStatictext7           33 // offset 0x133BA00
+#define iLeddigits4            34 // offset 0x133C800
+#define iiLeddigits4           35 // offset 0x1343600
+#define iStatictext8           36 // offset 0x1354600
 
 
 static const bool inputs[] PROGMEM = { false, false, false, false, false, false, false, false, 
