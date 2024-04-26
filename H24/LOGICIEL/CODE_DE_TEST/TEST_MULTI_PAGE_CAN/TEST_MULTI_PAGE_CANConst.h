5322ef7235a80fb69a1bd5676cbb56e90c9392cb
FFXIII <201604617@cegepsherbrooke.qc.ca>
2024-04-26 13:40:30 -0400

    Nouveau code

---
+++ H24/LOGICIEL/CODE_DE_TEST/TEST_MULTI_PAGE_CAN/TEST_MULTI_PAGE_CANConst.h
@@ -0,0 +1,15 @@
+// File generated 2024-04-26 13:04:15
+// Warning! This is a generated file, any manual changes will be
+// lost during the next generation.
+
+#define  IFONT_OFFSET     0
+// object indexes into ImageControl
+#define iLeddigits1             0 // offset 0x0
+#define iiLeddigits1            1 // offset 0xE400
+#define iLeddigits2             2 // offset 0x31E00
+#define iiLeddigits2            3 // offset 0x48000
+#define iWinbutton1             4 // offset 0x7F800
+#define iWinbutton2             5 // offset 0x8A600
+
+
+static const bool inputs[] PROGMEM = { false, false, false, false, true, true} ;
