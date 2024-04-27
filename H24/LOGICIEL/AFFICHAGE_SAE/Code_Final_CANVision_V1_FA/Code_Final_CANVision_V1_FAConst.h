024d75de4e202722ce1bd8a438c48f7fd9cb3d9f
FelixDuck <faguimont15@gmail.com>
2024-04-27 15:19:54 -0400

    Code incrémentation avec Affichage Final

---
+++ H24/LOGICIEL/AFFICHAGE_SAE/Code_Final_CANVision_V1_FA/Code_Final_CANVision_V1_FAConst.h
@@ -0,0 +1,56 @@
+// File generated 4/26/2024 3:18:24 PM
+// Warning! This is a generated file, any manual changes will be
+// lost during the next generation.
+
+#define  IFONT_OFFSET     0
+// object indexes into ImageControl
+#define iAngularmeter1          0 // offset 0x0
+#define iLeddigits1             1 // offset 0x75F400
+#define iiLeddigits1            2 // offset 0x761C00
+#define iStatictext1            3 // offset 0x76E000
+#define iStatictext2            4 // offset 0x76F200
+#define iLeddigits2             5 // offset 0x770A00
+#define iiLeddigits2            6 // offset 0x773200
+#define iStatictext3            7 // offset 0x77FA00
+#define iMeter1                 8 // offset 0x781E00
+#define iStatictext4            9 // offset 0xF31A00
+#define iTank1                 10 // offset 0xF32E00
+#define iLeddigits3            11 // offset 0x12E0E00
+#define iiLeddigits3           12 // offset 0x12E5000
+#define iLeddigits4            13 // offset 0x12F2A00
+#define iiLeddigits4           14 // offset 0x12F6C00
+#define iStatictext5           15 // offset 0x1304600
+#define iAngularmeter3         16 // offset 0x1305A00
+#define iAngularmeter4         17 // offset 0x19ABE00
+#define iLeddigits5            18 // offset 0x2052200
+#define iiLeddigits5           19 // offset 0x2054A00
+#define iLeddigits6            20 // offset 0x2060E00
+#define iiLeddigits6           21 // offset 0x2063600
+#define iLeddigits7            22 // offset 0x206FA00
+#define iiLeddigits7           23 // offset 0x2072200
+#define iStatictext6           24 // offset 0x207E600
+#define iStatictext7           25 // offset 0x2080A00
+#define iStatictext8           26 // offset 0x2082200
+#define iStatictext9           27 // offset 0x2083E00
+#define iAngularmeter2         28 // offset 0x2088200
+#define iStatictext10          29 // offset 0x272E600
+#define iStatictext11          30 // offset 0x272F800
+#define iStatictext12          31 // offset 0x2731000
+#define iStatictext13          32 // offset 0x2731E00
+#define iMeter2                33 // offset 0x2733C00
+#define iAngularmeter5         34 // offset 0x2EE3800
+#define iLeddigits8            35 // offset 0x34DA000
+#define iiLeddigits8           36 // offset 0x34DCC00
+#define iLeddigits9            37 // offset 0x34EA600
+#define iiLeddigits9           38 // offset 0x34ED200
+#define iLeddigits10           39 // offset 0x34FAC00
+#define iiLeddigits10          40 // offset 0x34FEE00
+#define iStatictext14          41 // offset 0x350C800
+
+
+static const bool inputs[] PROGMEM = { false, false, false, false, false, false, false, false, 
+                  false, false, false, false, false, false, false, false, 
+                  false, false, false, false, false, false, false, false, 
+                  false, false, false, false, false, false, false, false, 
+                  false, false, false, false, false, false, false, false, 
+                  false, false} ;
